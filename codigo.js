5970eea0a06d9d0dd9120d0353133a5a57d66e7e
Rafaela <Rafaelasousa201397@gmail.com>
2025-04-19 18:32:44 -0300

    firts commit

---
+++ codigo.js
@@ -0,0 +1,78 @@
+console.log("ola mundo");
+
+// variaveis 
+
+
+
+var numero = 5;
+var nome = "Amanda";
+var nome2 = "Victor";
+var dirige = true;
+
+console.log(numero,nome,dirige);
+
+var idade  = 19;
+var nome = "Amanda";
+var dirige = true;
+ 
+console.log(`O nome informado foi ${nome}`); // templete string = $ (é usado para fazer concatenação,ou seja juntar)
+console.log("o nome informado foi  "  + nome2);
+console.log(`O nome informado foi ${nome}, a idade é ${idade}. dirige:${dirige}`);
+
+
+// variaveis
+
+// Primeiro exemplo
+
+var n1 = 8;
+var n2 = 10;
+
+var soma = n1 + n2;
+
+console.log(`resultado é : ${soma}`);
+
+
+// Segundo exemplo
+
+var nome = "Victor";
+var n1 = 6;
+var n2 = 6.6;
+var n3 = 7.8;
+var n4 = 9;
+
+var media = ( n1 + n2 + n3 + n4) / 4;
+
+console.log(`A media é ${media}`);
+
+
+// Segundo exemplo
+
+var a = 40;
+var b = 30;
+var c = 20;
+var d = 42;
+var e = 30;
+var f = 60;
+var g = 30;
+var h = 20;
+
+var soma = a + b;
+var subtracao = c - d;
+var multiplicacao = 30 * 60;
+var divisao = 30 / 20;
+
+console.log(`A soma da numeros é ${soma}`);
+console.log(`O resultado da subtracao é ${subtracao}`);
+console.log(`O resultado da multiplicação é ${multiplicacao}`);
+console.log(`O resulatdo da divisao é ${divisao}`);
+
+
+
+
+
+
+
+
+
+
+
